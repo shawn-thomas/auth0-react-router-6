f59a59df9e361a3ba5ad0657f98b5879592d9930
Shawn <sljt@proton.me>
2024-02-09 22:36:01 -0500

    render PageLoader component while React Auth0 SDK is loading

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { PageLoader } from "./components/page-loader";
 import { AdminPage } from "./pages/admin-page";
 import { CallbackPage } from "./pages/callback-page";
 import { HomePage } from "./pages/home-page";
@@ -9,6 +11,16 @@ import { ProtectedPage } from "./pages/protected-page";
 import { PublicPage } from "./pages/public-page";
 
 export const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <div className="page-layout">
+        <PageLoader />
+      </div>
+    );
+  }
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
